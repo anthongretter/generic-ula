5e6fcb72dcd45c37ee2e7e87d8565c24c3a6f967
anthongretter <anthonporathgretter@yahoo.com.br>
2022-03-08 20:42:50 -0300

    divisor versao process

--- divisor/divisor_grupo.vhd
+++ divisor/divisor_grupo.vhd
@@ -1,36 +1,48 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+use ieee.numeric_std.all;
 
 entity divisor_grupo is
-    generic (N : integer);
-    port (clk, Reset, inicio: in std_logic;
+    generic (N : integer := 4);
+    port (reset, inicio: in std_logic;
         entA, entB : in std_logic_vector(N-1 downto 0);
-        saida, resto: out std_logic_vector(2*N-1 downto 0);
-        pronto: out std_logic);
+        quociente, resto: out std_logic_vector(N-1 downto 0);
+        pronto, erro: out std_logic);
 end divisor_grupo;
 
 architecture arc of divisor_grupo is
-    -- componentes (bo e bc)
+    signal quo, res, zeros : std_logic_vector(N-1 downto 0) := (others => '0');
 
-    component bc_div is
-        port (Reset, clk, inicio : in std_logic;
-            A_zero, B_zero: in std_logic;
-            pronto, erro: out std_logic;
-            carga_Entradas, carga_mult, mux_B, mux_mult: out std_logic);
-    end component;
-  
-    component bo_div is
-        generic (N : integer);
-        port (clk : in std_logic;
-            carga_Entradas, mux_B, carga_mult, mux_mult : in std_logic;
-            entA, entB : in std_logic_vector(N - 1 downto 0);
-            Az, Bz: out std_logic;
-            mult : out std_logic_vector(2*N - 1 downto 0));
-    end component;
+begin
+    P : process(inicio)
+        variable i : integer;
+    begin
+        if (inicio = '1') then
+            i := N - 1;
+            pronto <= '0';
+            erro <= '0';
 
-    -- sinais de saida bc
-    signal carga_Entradas, carga_mult, mux_B, mux_mult: std_logic;
+            L : while i > -1 loop
+					if (entB = zeros) then
+						 erro <= '1';
+						 exit L;
+					end if;
+				
+                res <= std_logic_vector(shift_left(unsigned(res), 1));
+                res(i) <= entA(i);
+                
+                if (res >= entB) then
+                    res <= res - entB;
+                    quo(i) <= '1';
+                end if;
+                i := i - 1;
+            end loop L;
+            pronto <= '1';
+        end if;
+    end process;
 
-    -- sinais de saida do bo
-    signal Az, Bz: std_logic;
+    quociente <= quo;
+    resto <= res;
+
 end arc;
